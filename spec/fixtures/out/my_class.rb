eb53a821ea4e8e1dcec5a72521ff2e3381dd555d
nic-lan <nicolas.languille@gmail.com>
2020-01-03 15:41:35 +0000

    integration test on Generate command

---
+++ spec/fixtures/out/my_class.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# The MyClass class is responsible for TODO
+class MyClass
+end
+
--- spec/lib/ruby_template/commands/generate_spec.rb
+++ spec/lib/ruby_template/commands/generate_spec.rb
@@ -5,12 +5,20 @@ require 'spec_helper'
 RSpec.describe RubyTemplate::Commands::Generate do
   describe '.perform' do
     let(:entity_name) { 'MyClass' }
-    let(:options) { { root: 'lib', name: entity_name, type: 'class', methods: [] } }
+    let(:root) { 'spec/fixtures/out' }
+    let(:options) { { root: root, name: entity_name, type: 'class', methods: [] } }
+
+    let(:fixture_filename) { 'my_class.rb' }
+    let(:expected_entity) { fixture(fixture_filename).read }
+
+    let(:result_filename) { "out/#{fixture_filename}" }
+    let(:result_entity) { fixture(result_filename).read }
 
     subject { described_class.call(entity_name, options) }
 
     it 'creates a class ruby file with the right name' do
       subject
+      expect(result_entity).to eq(result_entity)
     end
   end
 end
