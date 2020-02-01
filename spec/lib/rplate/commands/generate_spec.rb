f9e5e675d384c1eff736a326decdbbedb70dde47
nic-lan <me@niclan.io>
2020-02-01 00:56:01 +0000

    Marshal.load Marshal.dump context_opts to avoid missing entity_constants due to `shift` firing on top of it

--- spec/lib/rplate/commands/generate_spec.rb
+++ spec/lib/rplate/commands/generate_spec.rb
@@ -20,35 +20,50 @@ RSpec.describe RPlate::Commands::Generate do
     subject { described_class.call(entity_name, options) }
 
     shared_examples 'create the required ruby files' do
-      let(:fixture_filename) { "#{entity_name.underscore}.rb" }
+      let(:base_filename) { entity_name.underscore }
+      let(:fixture_filename) { "#{base_filename}.rb" }
       let(:expected_entity) { fixture(fixture_filename) }
 
       let(:result_filename) { "out/#{fixture_filename}" }
       let(:result_entity) { fixture(result_filename, prefix_path: false) }
 
-      let(:fixture_filename_spec) { "spec/#{entity_name.underscore}_spec.rb" }
+      let(:fixture_filename_spec) { "spec/#{base_filename}_spec.rb" }
       let(:expected_entity_spec) { fixture(fixture_filename_spec) }
 
-      let(:result_filename_spec) { "spec/#{root}/#{entity_name.underscore}_spec.rb" }
+      let(:result_filename_spec) { "spec/#{root}/#{base_filename}_spec.rb" }
       let(:result_entity_spec) { fixture(result_filename_spec, prefix_path: false) }
 
+      before { subject }
+
       it 'creates the entity ruby file' do
-        subject
         expect(result_entity).to eq(expected_entity)
       end
 
       it 'creates the related spec file' do
-        subject
         expect(result_entity_spec).to eq(expected_entity_spec)
       end
     end
 
     it_behaves_like 'create the required ruby files'
 
-    context 'when the directories in the entity or root are not existing' do
+    context 'when the given entity has a namespaced class name by ::' do
       let(:entity_name) { 'MyModule::MyClass' }
 
       it_behaves_like 'create the required ruby files'
+    end
+
+    context 'when the given entity name is underscored and splittable by /' do
+      let(:entity_name) { 'my_module/my_class' }
+
+      it_behaves_like 'create the required ruby files'
+    end
+
+    context 'when the given entity name is underscored and splittable by :' do
+      let(:entity_name) { 'my_module:my_class' }
+
+      it_behaves_like 'create the required ruby files' do
+        let(:base_filename) { 'my_module/my_class' }
+      end
     end
 
     context 'when some methods and namespaces' do
