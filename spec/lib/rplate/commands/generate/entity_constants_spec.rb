6b7deb21c85d133244711a89d82683aea935097e
nic-lan <me@niclan.io>
2020-02-01 00:48:04 +0000

    Add EntityConstants class to improve Single on fetching the entity constants from an entity name

---
+++ spec/lib/rplate/commands/generate/entity_constants_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RPlate::Commands::Generate::EntityConstants do
+  describe '.fetch' do
+    let(:entity_name) { 'MyClass' }
+    let(:entity) { double(:entity, name: entity_name) }
+
+    subject { described_class.fetch(entity) }
+
+    it { is_expected.to eq(%w[MyClass]) }
+
+    context 'when constants splitted by :: are given' do
+      let(:entity_name) { 'MyModule::MyClass' }
+
+      it { is_expected.to eq(%w[MyModule MyClass]) }
+    end
+
+    context 'when underscored class name is given' do
+      let(:entity_name) { 'my_class' }
+
+      it { is_expected.to eq(%w[MyClass]) }
+    end
+
+    context 'when underscored class name splittable by `/` is given' do
+      let(:entity_name) { 'my_module/my_class' }
+
+      it { is_expected.to eq(%w[MyModule MyClass]) }
+    end
+
+    context 'when underscored class name splittable by `:` is given' do
+      let(:entity_name) { 'my_module:my_class' }
+
+      it { is_expected.to eq(%w[MyModule MyClass]) }
+    end
+  end
+end
