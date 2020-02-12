57621f6ff494e74456a3bab709b4e22c82893600
nic-lan <me@niclan.io>
2020-02-12 00:18:07 +0000

    move from Hanami::validations to dry-validations

--- lib/rplate/commands/generate/validate_params.rb
+++ lib/rplate/commands/generate/validate_params.rb
@@ -5,25 +5,23 @@ module RPlate
     class Generate
       #
       # Generate is the ValidateParams for the Generate Command
-      class ValidateParams
-        include Hanami::Validations
-
-        ALLOWED_NAME_REGEX = EntityConstants::ALLOWED_NAME_REGEX
+      class ValidateParams < Dry::Validation::Contract
+        ALLOWED_NAME_REGEX = /\A[a-z][a-z0-9_]*\z/.freeze
         ALLOWED_TYPES = %w[class module].freeze
         ALLOWED_METHODS_REGEX = /(self.)?([a-z]+_?)/.freeze
+        ALLOWED_INFLECTIONS_REGEX = /[a-z]\w*:[A-Z]\w*/.freeze
 
-        validations do
-          required(:name) { str? & format?(ALLOWED_NAME_REGEX) }
-          required(:type) { str? & included_in?(ALLOWED_TYPES) }
-          required(:required_methods) do
-            array? { each { str? & format?(ALLOWED_METHODS_REGEX) } }
-          end
+        params do
+          # required(:name).array(:filled?, format?: ALLOWED_NAME_REGEX)
+          required(:name).value(:array, min_size?: 1).each(:str?, format?: ALLOWED_NAME_REGEX)
+          required(:type).filled(included_in?: ALLOWED_TYPES)
+          required(:required_methods).array(:str?, format?: ALLOWED_METHODS_REGEX)
+          required(:inflections).array(:str?, format?: ALLOWED_INFLECTIONS_REGEX)
+          optional(:root)
         end
 
         def self.call(class_name, options)
-          options = options.deep_symbolize_keys.merge(name: class_name)
-
-          new(options).validate
+          new.call options.merge(name: class_name)
         end
       end
     end
--- spec/lib/rplate/commands/generate/validate_params_spec.rb
+++ spec/lib/rplate/commands/generate/validate_params_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe RPlate::Commands::Generate::ValidateParams do
   describe '.call' do
     subject { described_class.call(class_name, options) }
 
-    let(:class_name) { 'Whatever' }
+    let(:class_name) { %w[whatever] }
     let(:type_value) { 'class' }
     let(:methods_value) { [] }
+    let(:inflections) { [] }
     let(:options) do
       { 'type' => type_value,
-        'required_methods' => methods_value }
+        'required_methods' => methods_value,
+        'inflections' => inflections }
     end
 
     it { is_expected.to be_success }
@@ -27,12 +29,12 @@ RSpec.describe RPlate::Commands::Generate::ValidateParams do
 
       it 'is not success' do
         expect(subject).not_to be_success
-        expect(subject.messages).to include(name: ['must be a string'])
+        expect(subject.errors.to_h).to include(name: ['must be an array'])
       end
     end
 
-    context 'when name is downcase' do
-      let(:class_name) { 'my_class' }
+    context 'when name is underscored' do
+      let(:class_name) { %w[my_class] }
 
       it 'is success' do
         expect(subject).to be_success
@@ -40,44 +42,41 @@ RSpec.describe RPlate::Commands::Generate::ValidateParams do
     end
 
     context 'when name is downcase with spaces' do
-      let(:class_name) { 'my_module my_class' }
-
-      it 'is not success' do
-        expect(subject).not_to be_success
-        expect(subject.messages).to include(name: ['is in invalid format'])
-      end
-    end
-
-    context 'when name is downcase with spaces' do
-      let(:class_name) { 'MyModule MyClass' }
-
-      it 'is not success' do
-        expect(subject).not_to be_success
-        expect(subject.messages).to include(name: ['is in invalid format'])
-      end
-    end
-
-    context 'when name is downcase and splitted by `:`' do
-      let(:class_name) { 'my_module/my_class' }
+      let(:class_name) { %w[my_module my_class] }
 
       it 'is success' do
         expect(subject).to be_success
+      end
+    end
+
+    context 'when name is camelized with spaces' do
+      let(:class_name) { %w[MyModule MyClass] }
+
+      it 'is not success' do
+        expect(subject).not_to be_success
+        expect(subject.errors.to_h).to include(
+          name: {
+            0 => ['is in invalid format'], 1 => ['is in invalid format']
+          }
+        )
+      end
+    end
+
+    context 'when name is downcase and splitted by `/`' do
+      let(:class_name) { %w[my_module/my_class] }
+
+      it 'is not success' do
+        expect(subject).not_to be_success
+        expect(subject.errors.to_h).to include(name: { 0 => ['is in invalid format'] })
       end
     end
 
     context 'when name is downcase with namespace with : separation' do
-      let(:class_name) { 'my_module:my_class' }
+      let(:class_name) { %w[my_module:my_class] }
 
-      it 'is success' do
-        expect(subject).to be_success
-      end
-
-      context 'when multiple nastings' do
-        let(:class_name) { 'my_module:my_class:sub' }
-
-        it 'is success' do
-          expect(subject).to be_success
-        end
+      it 'is not success' do
+        expect(subject).not_to be_success
+        expect(subject.errors.to_h).to include(name: { 0 => ['is in invalid format'] })
       end
     end
 
@@ -86,25 +85,25 @@ RSpec.describe RPlate::Commands::Generate::ValidateParams do
 
       it 'is not success' do
         expect(subject).not_to be_success
-        expect(subject.messages).to include(name: ['is in invalid format'])
+        expect(subject.errors.to_h).to include(name: ['size cannot be less than 1'])
       end
     end
 
     context 'when name is start with number and underscore' do
-      let(:class_name) { '2class' }
+      let(:class_name) { %w[2class] }
 
       it 'is not success' do
         expect(subject).not_to be_success
-        expect(subject.messages).to include(name: ['is in invalid format'])
+        expect(subject.errors.to_h).to include(name: { 0 => ['is in invalid format'] })
       end
     end
 
     context 'when name is start with number' do
-      let(:class_name) { '2Class' }
+      let(:class_name) { %w[2Class] }
 
       it 'is not success' do
         expect(subject).not_to be_success
-        expect(subject.messages).to include(name: ['is in invalid format'])
+        expect(subject.errors.to_h).to include(name: { 0 => ['is in invalid format'] })
       end
     end
 
@@ -113,7 +112,7 @@ RSpec.describe RPlate::Commands::Generate::ValidateParams do
 
       it 'is not success' do
         expect(subject).not_to be_success
-        expect(subject.messages).to include(type: ['must be one of: class, module'])
+        expect(subject.errors.to_h).to include(type: ['must be one of: class, module'])
       end
     end
 
@@ -133,6 +132,38 @@ RSpec.describe RPlate::Commands::Generate::ValidateParams do
       let(:methods_value) { ['perform', 'self.class_method'] }
 
       it { is_expected.to be_success }
+    end
+
+    context 'when inflections is one valid element' do
+      let(:inflections) { ['rplate:RPlate'] }
+
+      it { is_expected.to be_success }
+    end
+
+    context 'when inflections is one valid element' do
+      let(:inflections) { ['rplate-RPlate'] }
+
+      it 'is not success' do
+        expect(subject).not_to be_success
+        expect(subject.errors.to_h).to include(
+          inflections: {
+            0 => ['is in invalid format']
+          }
+        )
+      end
+    end
+
+    context 'when inflections is one invalid element' do
+      let(:inflections) { ['RPlate:rplate'] }
+
+      it 'is not success' do
+        expect(subject).not_to be_success
+        expect(subject.errors.to_h).to include(
+          inflections: {
+            0 => ['is in invalid format']
+          }
+        )
+      end
     end
   end
 end
